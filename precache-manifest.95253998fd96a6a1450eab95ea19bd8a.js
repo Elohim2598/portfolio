e7d7350c9f4112015d82c3120ccdf20c5ac75afe
Elohim2598 <elohim2598@gmail.com>
2022-06-08 12:21:20 -0400

    Updates

---
+++ precache-manifest.95253998fd96a6a1450eab95ea19bd8a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "a95f44b3fd9943312846",
+    "url": "/portfolio/static/css/main.da51efe9.chunk.css"
+  },
+  {
+    "revision": "a95f44b3fd9943312846",
+    "url": "/portfolio/static/js/main.a95f44b3.chunk.js"
+  },
+  {
+    "revision": "51d72351afee2f4d4da9",
+    "url": "/portfolio/static/js/1.51d72351.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "6fd89776729cc45075b56ea2b08e761c",
+    "url": "/portfolio/static/media/me.6fd89776.jpg"
+  },
+  {
+    "revision": "7de371b5a116e736644e77f756e01255",
+    "url": "/portfolio/static/media/cryptochecker.7de371b5.png"
+  },
+  {
+    "revision": "3e55724bb13507e17e11f845bbb2d831",
+    "url": "/portfolio/static/media/foodrecipesapp.3e55724b.png"
+  },
+  {
+    "revision": "0cd580f1fbd0d2e61c647b85f7058a37",
+    "url": "/portfolio/static/media/newsapp1.0cd580f1.png"
+  },
+  {
+    "revision": "b6d10e7cc5a8a5a43e49f7b90cc4f418",
+    "url": "/portfolio/static/media/newsapp2.b6d10e7c.png"
+  },
+  {
+    "revision": "9afab20b70f9cd6249751946d0be23c5",
+    "url": "/portfolio/static/media/memegenerator.9afab20b.png"
+  },
+  {
+    "revision": "7aab568ca549b65135b734438e5c7558",
+    "url": "/portfolio/static/media/weatherapp.7aab568c.png"
+  },
+  {
+    "revision": "b848d5d9bb221592064de0f356f61676",
+    "url": "/portfolio/static/media/css.b848d5d9.png"
+  },
+  {
+    "revision": "a2bc8ecf6d9052818b36402f0431c60f",
+    "url": "/portfolio/static/media/sass.a2bc8ecf.png"
+  },
+  {
+    "revision": "63cf9c6f8153aef1e884b7ae2815e06d",
+    "url": "/portfolio/static/media/javascript.63cf9c6f.png"
+  },
+  {
+    "revision": "f297dfcd8b3e9215ee862f232d5c9d13",
+    "url": "/portfolio/static/media/react.f297dfcd.png"
+  },
+  {
+    "revision": "bff6b2788849ed735b91799212e1d4c4",
+    "url": "/portfolio/static/media/typescript.bff6b278.png"
+  },
+  {
+    "revision": "50a729cbf01055de07982ec9d064467c",
+    "url": "/portfolio/static/media/resumebg.50a729cb.jpg"
+  },
+  {
+    "revision": "b5841b1c291242f4b33e349adeeaaf97",
+    "url": "/portfolio/index.html"
+  }
+];
